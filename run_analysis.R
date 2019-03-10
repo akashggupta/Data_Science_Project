555223abd22aa1f2fb1dd2d312a4ada677480a4f
Akash Gupta <42721805+akashggupta@users.noreply.github.com>
2019-03-10 15:14:44 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,16 @@
+library(dplyr)
+
+pathdata = file.path("C:/Users/user/Documents/test/ucr", "UCI HAR Dataset")
+### 1. Output Steps - Here we begin how to create the data set of training and test
+#Reading training tables - xtrain / ytrain, subject train
+xtrain = read.table(file.path(pathdata, "train", "X_train.txt"),header = FALSE)
+ytrain = read.table(file.path(pathdata, "train", "y_train.txt"),header = FALSE)
+subject_train = read.table(file.path(pathdata, "train", "subject_train.txt"),header = FALSE)
+#Reading the testing tables
+xtest = read.table(file.path(pathdata, "test", "X_test.txt"),header = FALSE)
+ytest = read.table(file.path(pathdata, "test", "y_test.txt"),header = FALSE)
+subject_test = read.table(file.path(pathdata, "test", "subject_test.txt"),header = FALSE)
+#Read the features data
+features = read.table(file.path(pathdata, "features.txt"),header = FALSE)
+#Read activity labels data
+activityLabels = read.table(file.path(pathdata, "activity_labels.txt"),header = FALSE)
